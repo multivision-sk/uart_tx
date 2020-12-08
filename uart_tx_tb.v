cd4f7055565034450ea5aa77accd2724c4c0ad79
SKK <67934680+multivision-sk@users.noreply.github.com>
2020-12-08 19:21:59 +0900

    Update uart_tx_tb.v

--- uart_tx_tb.v
+++ uart_tx_tb.v
@@ -1,21 +1,3 @@
-// ******************************************************************************
-                                                                             // *
-                  // Copyright (C) 2009 Altera Corporation                      *
-                                                                             // *
- // ALTERA, ARRIA, CYCLONE, HARDCOPY, MAX, MEGACORE, NIOS, QUARTUS & STRATIX    *
- // are Reg. U.S. Pat. & Tm. Off. and Altera marks in and outside the U.S.      *
-                                                                             // *
- // All information provided herein is provided on an "as is" basis,            *
- // without warranty of any kind.                                               *
-                                                                             // *
- // Module Name: counter_tb                    File Name: counter_tb.v          *
-                                                                             // *
- // Module Function: This file contains the testbench for the Introduction      *
- // 					to Verilog lab 4b		  		   			            *
-                                                                             // *
- // REVISION HISTORY:                                                           *
-  // Revision 1.0    12/15/2009 - Initial Revision  for QII 9.1                 *
-// ******************************************************************************
 
 `timescale 1 ns/1 ns
 
